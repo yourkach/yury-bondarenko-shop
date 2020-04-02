305b43fdd1d700d1d180c2817cc13cc51b4801ab
YuryBondarenko <xisofix@gmail.com>
2020-04-02 21:46:25 +0500

    added three classes implementing PricePrinter interface in different ways

--- app/src/test/java/com/example/yury_bondarenko_shop/ExampleUnitTest.kt
+++ app/src/test/java/com/example/yury_bondarenko_shop/ExampleUnitTest.kt
@@ -1,7 +1,6 @@
 package com.example.yury_bondarenko_shop
 
 import org.junit.Test
-
 import kotlin.math.pow
 import kotlin.math.round
 
@@ -11,16 +10,17 @@ class ExampleUnitTest {
     fun example() {
 
         val iphoneCase = Product(price = 123.5, salePercent = 30)
-
-        var pricePrinter: PricePrinter = FirstPricePrinter()
-
         val discountIphoneCasePrice = iphoneCase.calcDiscountPrice()
+
+        var pricePrinter: PricePrinter
+
+        pricePrinter = SimplePricePrinter()
         pricePrinter.print(discountIphoneCasePrice)
 
-        pricePrinter = SecondPricePrinter()
+        pricePrinter = MeasuredPricePrinter("чехол")
         pricePrinter.print(discountIphoneCasePrice)
 
-        pricePrinter = ThirdPricePrinter()
+        pricePrinter = CurrencyPricePrinter("$")
         pricePrinter.print(discountIphoneCasePrice)
     }
 }
@@ -45,33 +45,47 @@ class Product(
 interface PricePrinter {
 
     /**
-     * Outputs price in <PRICE>P format.
+     * Outputs formatted price on new console line
      * If price have not fractional part than it will be printed as integer
      * If price have fractional part than it will be rounded for 2 symbols after "."
      */
     fun print(price: Double)
 }
 
-class FirstPricePrinter : PricePrinter {
+class SimplePricePrinter : PricePrinter {
+    /**
+     * Outputs only formatted price on new console line
+     * !!вопрос, стоит ли дублировать документацию интерфейса в документации его реализации?
+     */
     override fun print(price: Double) {
-        if (price % 1.0 == 0.0) println("${price.toInt()}P")
-        else println("${price.roundTo(2)}P")
-    }
-}
-
-class SecondPricePrinter : PricePrinter {
-    override fun print(price: Double) {
-        when (price % 1.0) {
-            0.0 -> println("${price.toInt()}P")
-            else -> println("${price.roundTo(2)}P")
+        when {
+            price.isWholeNumber() -> println("${price.toInt()}")
+            else -> println("${price.roundTo(2)}")
         }
     }
 }
 
-class ThirdPricePrinter : PricePrinter {
-    override fun print(price: Double) = when (round(price)) {
-        price -> println("${price.toInt()}P")
-        else -> println("${price.roundTo(2)}P")
+class MeasuredPricePrinter(private val measure: String = "шт") : PricePrinter {
+    /**
+     * Outputs price in <PRICE>/<MEASURE> format on new console line
+     */
+    override fun print(price: Double) {
+        when {
+            price.isWholeNumber() -> println("${price.toInt()}/$measure")
+            else -> println("${price.roundTo(2)}/$measure")
+        }
+    }
+}
+
+class CurrencyPricePrinter(private val currency: String = "Р") : PricePrinter {
+    /**
+     * Outputs price in <PRICE><CURRENCY> format on new console line
+     */
+    override fun print(price: Double) {
+        when {
+            price.isWholeNumber() -> println("${price.toInt()}$currency")
+            else -> println("${price.roundTo(2)}$currency")
+        }
     }
 }
 
@@ -82,3 +96,8 @@ fun Double.roundTo(decimals: Int): Double {
     val multiplier = (10.0).pow(decimals)
     return round(this * multiplier) / multiplier
 }
+
+/**
+ * Returns `true` if the number is whole (integer value), `false` otherwise.
+ */
+fun Double.isWholeNumber(): Boolean = this % 1.0 == 0.0
